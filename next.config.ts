b805e873b453715be1c261f349f9d15e0ba4ee1b
Mafaz Abrar Jan Chowdhury <mafazabrar@gmail.com>
2025-03-06 19:21:40 +1100

    testing

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
     unoptimized: true, // Disable default image optimization
   },
   output: 'export',
-  basePath: './',
-  assetPrefix: './',
+  basePath: '/mafaz-abrar.github.io',
+  assetPrefix: '/mafaz-abrar.github.io',
 };
 
 export default nextConfig;
