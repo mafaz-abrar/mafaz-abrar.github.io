7c3e62ca027626d587d73d32de1c6bdf3aa6b62e
Mafaz Abrar Jan Chowdhury <mafazabrar@gmail.com>
2025-03-06 18:46:27 +1100

    Add nojekyll

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: '/mafaz-abrar.github.io/',
+  basePath: '/mafaz-abrar.github.io',
+  output: 'export',
 };
 
 export default nextConfig;
