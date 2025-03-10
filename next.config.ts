421d1482b1443c343274903385b6540f5bc7c44d
Mafaz Abrar Jan Chowdhury <mafazabrar@gmail.com>
2025-03-10 16:30:42 +1100

    Debug github actions

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from 'next';
 
-const isProduction: boolean = process.env.NODE_ENV == 'production';
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
   output: 'export',
 };
 
